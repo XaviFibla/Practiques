8b49023c9d6fcf99a3b93c072f62f37bb69406b2
XaviFibla <xavi.fibla@gmail.com>
2022-12-14 11:32:06 +0100

    bootstrap-table

--- practica 6/projecte.php
+++ practica 6/projecte.php
@@ -8,7 +8,7 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.css">
+    <link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.21.2/dist/bootstrap-table.min.css">
 
     <title>Hello, world!</title>
 </head>
@@ -27,11 +27,20 @@
     </form>
     <br />
     <div id="content"></div>
+    <table id="table" data-toggle="table" data-height="460" data-ajax="ajaxRequest">
+        <thead>
+            <tr>
+                <th data-field="id">ID</th>
+                <th data-field="name">Name</th>
+                <th data-field="progress">Progress</th>
+            </tr>
+        </thead>
+    </table>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
+    <script src="https://unpkg.com/bootstrap-table@1.21.2/dist/bootstrap-table.min.js"></script>
 </body>
 
 </html>
@@ -64,12 +73,14 @@
         });
     });
 
-    /*const fetchPromise = fetch("http://localhost:3000/projects.php");
-    fetchPromise.then(response => {
-        return response.json();
-    }).then(projects => {
-        console.log(projects);
-    });*/
+
+    // your custom ajax request here
+    function ajaxRequest(params) {
+        var url = 'http://localhost:3000/projects.php'
+        $.get(url).then(function(res) {
+            params.success(JSON.parse(res))
+        })
+    }
 </script>
 </body>
 
